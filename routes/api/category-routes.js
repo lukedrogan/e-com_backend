429b2757af00c0bd8b0200b632964813a7094f03
Luke Drogan <luke@super-wide.com>
2021-06-24 16:09:41 +0100

    updated routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,18 +16,31 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then((categories) => {
+    res.json(categories)
+  })
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
-});
+  Category.create(req.body).then((category) => {
+    res.status(200).json(category)
+  })
+})
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  Category.update({category_name: req.body.category_name});
+})
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
 
-module.exports = router;
+module.exports = router
